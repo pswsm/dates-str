63b5837bc8f70e7829704bde03fac873d50fb32a
pswsm <53122461+pswsm@users.noreply.github.com>
2025-02-01 21:25:29 +0100

    fix the Add and Sub implementations for DateStr and clear code

--- src/impls.rs
+++ src/impls.rs
@@ -1,12 +1,12 @@
 #![deny(missing_docs)]
 
-use crate::DateStr;
+use crate::{DateStr, errors::DateErrors};
 use std::ops::{Add, Sub};
 
 /// Trait for easy DateStr making
 ///
 /// Blank implementation
-pub trait ToDateStr {
+pub trait Into<DateStr> {
     /// This function creates a [crate::DateStr] in a to_string() fashion
     fn to_datestr(&self) -> DateStr;
 
@@ -16,7 +16,7 @@ pub trait ToDateStr {
 }
 
 /// Implementation of ToDateStr for String
-impl ToDateStr for String {
+impl Into<DateStr> for String {
     fn to_datestr(&self) -> DateStr {
         DateStr::from_iso_str(self)
     }
@@ -27,7 +27,7 @@ impl ToDateStr for String {
 }
 
 /// Implementation of ToDateStr for &str
-impl ToDateStr for str {
+impl Into<DateStr> for str {
     fn to_datestr(&self) -> DateStr {
         DateStr::from_iso_str(self)
     }
@@ -37,25 +37,24 @@ impl ToDateStr for str {
     }
 }
 
-/// Implementation of ToDateStr for DateStr
-impl ToDateStr for DateStr {
-    fn to_datestr(&self) -> DateStr {
-        self.clone()
-    }
+impl TryFrom<String> for DateStr {
+    type Error = DateErrors;
 
-    fn try_to_datestr(&self) -> Result<DateStr, crate::errors::DateErrors> {
-        DateStr::try_from_iso_str(self)
-    }
-}
-
-impl From<String> for DateStr {
-    fn from(value: String) -> Self {
+    fn try_from(value: String) -> Result<Self, Self::Error> {
         let split: Vec<String> = value.split('-').map(|s| s.to_string()).collect();
-        DateStr {
-            year: split[0].parse::<u64>().unwrap_or(2020),
-            month: split[1].parse::<u8>().unwrap_or(0),
-            day: split[2].parse::<u8>().unwrap_or(0),
-        }
+        let year = match split[0].parse::<u64>() {
+            Ok(y) => crate::Year::new(y),
+            Err(_e) => return Err(DateErrors::InvalidParsing(value)),
+        };
+        let month = match split[1].parse::<u8>() {
+            Ok(y) => crate::Month::new(y)?,
+            Err(_e) => return Err(DateErrors::InvalidParsing(value)),
+        };
+        let day = match split[2].parse::<u8>() {
+            Ok(y) => crate::Day::new(y)?,
+            Err(_e) => return Err(DateErrors::InvalidParsing(value)),
+        };
+        Ok(DateStr { year, month, day })
     }
 }
 
@@ -68,26 +67,10 @@ impl From<DateStr> for String {
 impl Add for DateStr {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
-        let (day, add_mo): (u8, u8) = {
-            let mut day = self.day + rhs.day;
-            let mut add_mo: u8 = 0;
-            while day > 31 {
-                day -= 31;
-                add_mo += 1;
-            }
-            (day, add_mo)
-        };
-        let (month, add_year): (u8, u64) = {
-            let mut months: u8 = self.month + rhs.month + add_mo;
-            let mut add_year: u64 = 0;
-            while months > 12 {
-                add_year += 1;
-                months -= 12;
-            }
-            (months, add_year)
-        };
-        let year = self.year + rhs.year + add_year;
-
+        let (day, months_from_day) = self.day + rhs.day;
+        let (months, years) = self.month + rhs.month;
+        let (month, more_years) = months + months_from_day;
+        let year = self.year + rhs.year + years + more_years;
         DateStr { day, month, year }
     }
 }
@@ -95,34 +78,10 @@ impl Add for DateStr {
 impl Sub for DateStr {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
-        let (day, sub_mo): (u8, u8) = {
-            let mut day = self.day as i8 - rhs.day as i8;
-            let mut sub_mo: u8 = 0;
-            while day < 1 {
-                if day == 0 {
-                    day += 1;
-                } else {
-                    day += 31;
-                }
-                sub_mo += 1;
-            }
-            (day as u8, sub_mo)
-        };
-        let (month, sub_year): (u8, u64) = {
-            let mut months: i8 = self.month as i8 + rhs.month as i8 + sub_mo as i8;
-            let mut sub_year: u64 = 0;
-            while months < 1 {
-                sub_year += 1;
-                if months == 0 {
-                    months += 1;
-                } else {
-                    months += 12;
-                }
-            }
-            (months as u8, sub_year)
-        };
-        let year = self.year - rhs.year - sub_year;
-
+        let (day, months_from_day) = self.day - rhs.day;
+        let (months, years) = self.month - rhs.month;
+        let (month, more_years) = months - months_from_day;
+        let year = self.year - rhs.year - years - more_years;
         DateStr { day, month, year }
     }
 }
